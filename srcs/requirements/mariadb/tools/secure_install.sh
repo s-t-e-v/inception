13caf6c29d03a9dde0ba760e72ff578c393c90e3
s-t-e-v <steven.bandaogo@proton.me>
2025-02-07 03:47:14 +0100

    enforce  + simplifies DB_ROOT_PASSWORD management

--- srcs/requirements/mariadb/tools/secure_install.sh
+++ srcs/requirements/mariadb/tools/secure_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TMP_CNF=/root/.my.cnf
-DB_ROOT_PSWD_FILE=/run/secrets/db_root_password
+DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 
 # Ensure the MySQL runtime directory exists and set correct permissions
 mkdir -p /run/mysqld
@@ -11,9 +11,9 @@ chown mysql:mysql /run/mysqld
 cat > "$TMP_CNF" <<EOF
 [client]
 user="root"
-password="$(cat $DB_ROOT_PSWD_FILE)"
+password="${DB_ROOT_PASSWORD}"
 EOF
-chmod 600 /root/.my.cnf
+chmod 600 "$TMP_CNF"
 
 mysqld&
 
@@ -23,17 +23,15 @@ until mysqladmin ping --silent; do
 done
 
 # Secure MariaDB
-DB_ROOT_PASSWORD=$(cat $DB_ROOT_PSWD_FILE)
-mysql --defaults-file=/root/.my.cnf -e "ALTER USER 'root'@'localhost' IDENTIFIED VIA mysql_native_password USING PASSWORD('${DB_ROOT_PASSWORD}');"
-mysql --defaults-file=/root/.my.cnf -e "DELETE FROM mysql.user WHERE User='';"
+mysql --defaults-file="$TMP_CNF" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';"
+mysql --defaults-file="$TMP_CNF" -e "DELETE FROM mysql.user WHERE User='';"
 # Disable Root Login from Remote Hosts
-mysql --defaults-file=/root/.my.cnf -e "DELETE FROM mysql.user WHERE User='root' AND Host='%';"
-mysql --defaults-file=/root/.my.cnf -e "DROP DATABASE IF EXISTS test;"
-mysql --defaults-file=/root/.my.cnf -e "FLUSH PRIVILEGES;"
+mysql --defaults-file="$TMP_CNF" -e "DELETE FROM mysql.user WHERE User='root' AND Host='%';"
+mysql --defaults-file="$TMP_CNF" -e "DROP DATABASE IF EXISTS test;"
+mysql --defaults-file="$TMP_CNF" -e "FLUSH PRIVILEGES;"
 
 # Stop MariaDB after securing it
-mysqladmin --defaults-file=/root/.my.cnf shutdown
+mysqladmin --defaults-file="$TMP_CNF" shutdown
 
 # Remove the password file immediately after setup
-rm -f /root/.my.cnf
-unset DB_ROOT_PASSWORD  # Remove password from memory
+rm -f "$TMP_CNF"
