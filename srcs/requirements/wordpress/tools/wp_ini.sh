3f69fb287bd18e1e1b6b6985b7decc90d9e7c2de
s-t-e-v <steven.bandaogo@proton.me>
2025-02-09 08:38:13 +0100

    wordpress container: add todo directives

--- srcs/requirements/wordpress/tools/wp_ini.sh
+++ srcs/requirements/wordpress/tools/wp_ini.sh
@@ -4,9 +4,11 @@ MYSQL_PASSWORD=$(cat "$MYSQL_PASSWORD_FILE")
 source "$WORDPRESS_CREDENTIALS_FILE"
 set +a
 
+# TODO: remove user listing
+# TODO: avoid Error displays for user creation
 wp-cli config create --allow-root --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="$WORDPRESS_DB_HOST" --locale="en_DB" 
 wp-cli core install --allow-root --url="$FQDN" --title="$WP_BLOG_TITLE" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_MAIL"
 wp-cli user create --allow-root "$WP_USER" "$WP_USER_MAIL" --user_pass="$WP_USER_PASSWORD" --role=author
-wp-cli user list --allow-root
+# wp-cli user list --allow-root
 
 tail -f
