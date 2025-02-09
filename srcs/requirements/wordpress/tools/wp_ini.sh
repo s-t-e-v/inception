0b611266bf909f2d6b7db725a22755ce9b4855ef
s-t-e-v <steven.bandaogo@proton.me>
2025-02-09 13:39:34 +0100

    wordpress container: working, launches php-fpm

--- srcs/requirements/wordpress/tools/wp_ini.sh
+++ srcs/requirements/wordpress/tools/wp_ini.sh
@@ -6,9 +6,13 @@ set +a
 
 # TODO: remove user listing
 # TODO: avoid Error displays for user creation
-wp-cli config create --allow-root --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="$WORDPRESS_DB_HOST" --locale="en_DB" 
+# FIXME: wordpress-1  | sh: 1: /usr/sbin/sendmail: not found on first wp init
+wp-cli config create --allow-root --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="$WORDPRESS_DB_HOST"
 wp-cli core install --allow-root --url="$FQDN" --title="$WP_BLOG_TITLE" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_MAIL"
 wp-cli user create --allow-root "$WP_USER" "$WP_USER_MAIL" --user_pass="$WP_USER_PASSWORD" --role=author
 # wp-cli user list --allow-root
 
-tail -f
+# tail -f
+# Start PHP-FPM in the foreground
+exec php-fpm8.3 -F
+# exec php-fpm -F
