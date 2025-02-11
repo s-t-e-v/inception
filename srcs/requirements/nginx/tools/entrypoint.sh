5bf2e3d16797f4ee107c45c064662da483d65b6b
sbandaog <sbandaog@student.42.fr>
2025-02-11 19:38:30 +0100

    rm check env FQDN and WEB_ROOT because already handled by docker-compose

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -12,10 +12,6 @@ chmod 600 ${CONF_DIR}/${FQDN}.key
 chmod 644 ${CONF_DIR}/${FQDN}.crt
 
 # Generate nginx.conf from template and environment variables
-if [ -z "$FQDN" ] || [ -z "$WEB_ROOT" ]; then
-    echo "Environment variables FQDN and WEB_ROOT must be set"
-    exit 1
-fi
 envsubst '$FQDN $WEB_ROOT' < "$CONF_DIR"/nginx.conf.template > "$CONF_DIR"/nginx.conf
 
 # Validate the configuration
