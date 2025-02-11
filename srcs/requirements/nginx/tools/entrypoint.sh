f45bb82790a5db1ed2b66b392fd38103786f2649
s-t-e-v <steven.bandaogo@proton.me>
2025-02-11 19:12:32 +0100

    make the project depends on WEB_ROOT env var + refac nginx container

---
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash/
+set -e
+
+# Generate ssl private key and contract
+echo "Generating SSL certificate..."
+if ! openssl req -newkey rsa:2048 -keyout ${CONF_DIR}/${FQDN}.key -x509 -days 90 -out ${CONF_DIR}/${FQDN}.crt -nodes -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=42inception/CN=${FQDN}"; then
+    echo "Failed to generate SSL certificate"
+    exit 1
+fi
+chown root:root ${CONF_DIR}/${FQDN}.key
+chmod 600 ${CONF_DIR}/${FQDN}.key
+chmod 644 ${CONF_DIR}/${FQDN}.crt
+
+# Generate nginx.conf from template and environment variables
+if [ -z "$FQDN" ] || [ -z "$WEB_ROOT" ]; then
+    echo "Environment variables FQDN and WEB_ROOT must be set"
+    exit 1
+fi
+envsubst '$FQDN $WEB_ROOT' < "$CONF_DIR"/nginx.conf.template > "$CONF_DIR"/nginx.conf
+
+# Validate the configuration
+nginx -t
+
+exec nginx -g 'daemon off;'
