c95cca716583d05c9d120d1e061dc16c58bfac10
s-t-e-v <steven.bandaogo@proton.me>
2025-02-17 05:32:35 +0100

    refac healthcheck redis + make redis container run as non-root

--- srcs/requirements/bonus/redis/tools/entrypoint.sh
+++ srcs/requirements/bonus/redis/tools/entrypoint.sh
@@ -5,22 +5,10 @@ set -a
 REDIS_PASSWORD=$(cat "$REDIS_PASSWORD_FILE")
 set +a
 
-# redis-server -v
-# ls /etc/redis/
+# Tweak Redis configuration
+echo "requirepass $REDIS_PASSWORD" >> /etc/redis/redis.conf
+echo "bind 0.0.0.0" >> /etc/redis/redis.conf
+echo "daemonize no" >> /etc/redis/redis.conf
 
-# cat /etc/redis/redis.conf | head -n 200
-
-sed -i "s/^# requirepass.*/requirepass $REDIS_PASSWORD/" \
-        /etc/redis/redis.conf
-sed -i 's/^bind .*/bind 0.0.0.0/' /etc/redis/redis.conf
-sed -i 's/^daemonize .*/daemonize no/' /etc/redis/redis.conf
-
-# cat /etc/redis/redis.conf | head -n 200
-
-# redis-server --version
-# redis-server --help | grep conf
-
-# exec redis-server
 echo "redis starting..."
-redis-server /etc/redis/redis.conf
-# tail -f
+exec redis-server /etc/redis/redis.conf
--- srcs/requirements/bonus/redis/tools/healthcheck.sh
+++ srcs/requirements/bonus/redis/tools/healthcheck.sh
@@ -3,4 +3,8 @@ set -e  # Exit if any command fails
 
 REDIS_PASSWORD=$(cat $REDIS_PASSWORD_FILE)
 
-redis-cli -a "$REDIS_PASSWORD" ping | grep "PONG" || exit 1
+if redis-cli -a "$REDIS_PASSWORD" ping | grep -q "PONG"; then
+  exit 0
+else
+  exit 1
+fi
