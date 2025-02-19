ddcffbce012adeaa2e9f6a33bf591c682a277bb1
sbandaog <sbandaog@student.42.fr>
2025-02-19 19:59:19 +0100

    vsftpd container v1

--- srcs/requirements/bonus/vsftpd/tools/entrypoint.sh
+++ srcs/requirements/bonus/vsftpd/tools/entrypoint.sh
@@ -13,6 +13,7 @@ echo "$VSFTPD_USER" > "$LOGIN_FILE"
 echo "$VSFTPD_PASSWORD" >> "$LOGIN_FILE"
 echo "" >> "$LOGIN_FILE"
 
+
 cd /etc/vsftpd
 txt2db.sh login.txt login.db
 cleanconf.sh
@@ -20,9 +21,9 @@ cd /
 
 echo "local_root=/var/www" > /etc/vsftpd/vsftpd_user_conf/"$VSFTPD_USER"
 echo "write_enable=YES" >> /etc/vsftpd/vsftpd_user_conf/"$VSFTPD_USER"
-echo "anon_upload_enable=YES" >> /etc/vsftpd/vsftpd_user_conf/"$VSFPD_USER"
+echo "anon_upload_enable=YES" >> /etc/vsftpd/vsftpd_user_conf/"$VSFTPD_USER"
 echo "anon_mkdir_write_enable=YES" >> /etc/vsftpd/vsftpd_user_conf/"$VSFTPD_USER"
-echo "anon_other_write_enable=YES" >> /etc/vsftpd/vsftpd_user_conf/"$VSFPD_USER"
+echo "anon_other_write_enable=YES" >> /etc/vsftpd/vsftpd_user_conf/"$VSFTPD_USER"
 
 echo "Starting vsftpd..."
 exec vsftpd /etc/vsftpd/vsftpd.conf
